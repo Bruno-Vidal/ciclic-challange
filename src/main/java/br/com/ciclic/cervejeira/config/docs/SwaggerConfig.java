f3820e73d4167fe99db26647a8701df7ebfbfb03
Bruno Vidal <bruno.vidal@opah.com.br>
2019-07-24 11:58:44 -0300

    Add Dock

--- src/main/java/br/com/ciclic/cervejeira/config/docs/SwaggerConfig.java
+++ src/main/java/br/com/ciclic/cervejeira/config/docs/SwaggerConfig.java
@@ -26,8 +26,8 @@ public class SwaggerConfig {
 
     private ApiInfo metaData() {
         return new ApiInfoBuilder()
-                .title("Spring Boot REST API")
-                .description("\"Spring Boot REST API for greeting people\"")
+                .title("Desafio Ciclic")
+                .description("\"Spring Boot REST API para gerenciamento de estilos de cerveja\"")
                 .version("1.0.0")
                 .license("Apache License Version 2.0")
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
